c5894c1062469dd08eb8f4f7436af239dd037230
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-11 22:59:00 -0700

    displays cards for default deck. need implement flip fn and select topic fn

--- flashcards-client/src/App.js
+++ flashcards-client/src/App.js
@@ -5,8 +5,8 @@ import FlashCardActions from './Models/FlashCardActions';
 
 function App() {
   const [availableTopics, setAvailableTopics] = useState([]);
-  const [currentTopic, setCurrentTopic] = useState('');
-  const [flashCardDeck, setFlashCardDeck] = useState([]);
+  const [currentTopic, setCurrentTopic] = useState(0);
+  const [currentDeck, setCurrentDeck] = useState([]);
 
   const updateTopics = async () => {
     try {
@@ -18,22 +18,31 @@ function App() {
     }
   };
 
-  // update state available topic list from API on page load
+  const updateDeck = async (topicStr) => {
+    try {
+      const response = await FlashCardActions.getTopicCards(topicStr);
+      // console.log(response.data);
+      setCurrentDeck(response.data);
+    } catch (error) {
+      return console.log(error);
+    }
+  };
+
+  // ONE PAGE LOAD: call API and get avail topics, then set deck from API call to first topic
   useEffect(() => {
-    updateTopics();
+    const asyncLoadTopicsAndDeck = async () => {
+      await updateTopics();
+      // console.log(availableTopics[0], 'first topic at page load');
+      updateDeck(availableTopics[0]);
+    };
+
+    // use fn to allow async for proper API call loading
+    asyncLoadTopicsAndDeck();
   }, []);
 
-  // header
-  // app title - credit
-  // dropdown with topic options
-
-  // main
   // display all cards from topic, with question and choices
   // each card can be clicked to flip and replace choices with answer
   // would be nice to add an (x) which records idx to an ignoredCards array in local storage
-
-  // footer
-  // credit to github with questions. link to benhammond.tech
 
   return (
     <div className="App flex flex-col h-screen">
@@ -44,7 +53,9 @@ function App() {
         </nav>
       </header>
       <main className="flex-1 overflow-y-auto p-5">
-        <FlashCard />
+        {currentDeck.map((card) => {
+          return <FlashCard card={card} />;
+        })}
       </main>
       <footer className="py-5 bg-gray-700 flex justify-center text-white p-5">
         <a href="https://benhammond.tech">benhammond.tech</a>
--- flashcards-client/src/components/FlashCard/FlashCard.jsx
+++ flashcards-client/src/components/FlashCard/FlashCard.jsx
@@ -1,5 +1,12 @@
-const FlashCard = () => {
-  return <div>a flashcard</div>;
+const FlashCard = ({ card }) => {
+  //   console.log(card);
+  return (
+    <div className="border-gray-700 rounded p-3 m-3 bg-purple-100 text-gray-900 ">
+      <small className="bg-gray-700 text-white">{card.topic}</small>
+      <pre className="break-words whitespace-pre-wrap font-bold">{card.question} ?</pre>
+      <pre className="break-words whitespace-pre-wrap">{card.choices}</pre>
+    </div>
+  );
 };
 
 export default FlashCard;
