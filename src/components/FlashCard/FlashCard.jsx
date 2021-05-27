5beb81f1cfe5dd6dd201fb6d895f2cda84313aeb
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-05-27 12:20:52 -0600

    fix css so lines appear

--- src/components/FlashCard/FlashCard.jsx
+++ src/components/FlashCard/FlashCard.jsx
@@ -11,13 +11,13 @@ const FlashCard = ({ card, idx }) => {
   return (
     <ReactCardFlip isFlipped={isFlipped}>
       {/* CARD FRONT  */}
-      <div key="front" className={`lined shadow-lg rounded p-3 m-10 lg:mx-60 `} onClick={flipCard}>
+      <div key="front" className="lined shadow-lg rounded p-3 m-10 lg:mx-60" onClick={flipCard}>
         <small className="bg-gray-700 text-white">{card.topic}</small>
         <pre className="break-words whitespace-pre-wrap font-bold">{card.question} ?</pre>
         <pre className="break-words whitespace-pre-wrap">{card.choices}</pre>
       </div>
       {/* CARD BACK WITH ANSWER  */}
-      <div key="back" className={`lined shadow-2xl rounded p-3 m-10 lg:mx-60 `} onClick={flipCard}>
+      <div key="back" className="lined shadow-2xl rounded p-3 m-10 lg:mx-60" onClick={flipCard}>
         <small className="bg-gray-700 text-white">{card.topic}</small>
         <pre className="break-words whitespace-pre-wrap font-bold text-blue-200">{card.question} ?</pre>
         <pre className="break-words whitespace-pre-wrap ">{card.answer}</pre>
